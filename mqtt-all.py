fb666ed4e456bc819e3341d77a42b9ba9b9fc31b
Lex Robinson <lex@lex.me.uk>
2021-10-24 15:38:02 +0100

    Fix up the source license comment

--- mqtt-all.py
+++ mqtt-all.py
@@ -1,5 +1,19 @@
 #!/usr/bin/env python3
 """
+   Copyright 2020 Robin Cole
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+
 Run mqtt broker on localhost: sudo apt-get install mosquitto mosquitto-clients
 
 Example run: python3 mqtt-all.py --broker 192.168.1.164 --topic enviro
